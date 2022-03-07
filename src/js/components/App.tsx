f5b91fb34d73f0ce6d487c5c5626093ef3d65484
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-03-07 01:01:35 -0500

    eslitn options alterted for ts

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -2,6 +2,6 @@ import { FunctionComponent } from "react";
 import "../../static/style.css";
 
 const App: FunctionComponent = () => {
-  return <div className="test">helo</div>;
+  return <div className="test">hello</div>;
 };
 export default App;
