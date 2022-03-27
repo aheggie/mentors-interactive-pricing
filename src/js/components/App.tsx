a5c8bbe038fb7c3cec0480b7c0d9c1d523ed6602
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-03-27 01:03:08 -0400

    github actions

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -2,6 +2,6 @@ import { FunctionComponent } from "react";
 import "../../static/style.css";
 
 const App: FunctionComponent = () => {
-  return <div className="test">hello</div>;
+  return <div className="test">hello there</div>;
 };
 export default App;
