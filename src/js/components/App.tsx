d8d423ba7ad7007163c7f14a99fb53e80f01db0b
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-03-28 23:44:34 -0400

    toggle written

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -2,7 +2,8 @@ import { FunctionComponent } from "react";
 import "../../static/style.css";
 
 //this is type string from svg.d.ts
-import IconCheck from "../../images/icon-check.svg";
+import IconCheck from "../../static/images/icon-check.svg";
+// import IconSlider from "../../static/images/icon-slider.svg";
 
 const App: FunctionComponent = () => {
   return (
@@ -20,6 +21,21 @@ const App: FunctionComponent = () => {
             <span className="pricecard__dollars">$16.00</span> /month
           </span>
           month
+        </div>
+        <input type="range" name="" id="" className="pricecard__range" />
+        {/* https://www.w3schools.com/howto/howto_css_switch.asp */}
+        <div className="pricecard__togglecontainer">
+          <span className="pricecard__monthlylabel">Monthly Billing</span>
+          <div className="pricecard__toggleswitch">
+            <input
+              type="checkbox"
+              name=""
+              id=""
+              className="pricecard__togglecheckbox"
+            />
+            <span className="pricecard__togglecircle"></span>
+          </div>
+          <span className="pricecard__yearlylabel">Yearly Billing</span>
         </div>
       </div>
       <div className="signupcard">
