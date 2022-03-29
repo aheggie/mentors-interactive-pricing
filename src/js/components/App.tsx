c80108ce164f5115415e59a52e99a9494755eff3
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-03-28 22:37:03 -0400

    header pricecard

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -2,6 +2,24 @@ import { FunctionComponent } from "react";
 import "../../static/style.css";
 
 const App: FunctionComponent = () => {
-  return <div className="test">hello there</div>;
+  return (
+    <>
+      <header className="header">
+        <h1 className="header__mainhead">Simple, traffic-based pricing</h1>
+        <h2 className="header__subhead">
+          Sign-up for our 30-day trial. No credit card required.
+        </h2>
+      </header>
+      <div className="pricecard">
+        <div className="pricecard__pricecontainer">
+          <span className="pricecard__pageviews">100k pageviews</span>
+          <span className="pricecard__rate">
+            <span className="pricecard__dollars"></span>$16.00
+          </span>
+          month
+        </div>
+      </div>
+    </>
+  );
 };
 export default App;
